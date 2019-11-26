d1789660df110d628eab6f3581b157a33608b80c
Jazmin M <jazminmerckel@gmail.com>
2019-11-26 02:58:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) AS value_occurrence FROM bears GROUP BY color ORDER BY value_occurrence DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
